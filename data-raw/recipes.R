fdec4f003e497f9a42e54e1229e26c89aa8f10e3
hadley <h.wickham@gmail.com>
2015-10-31 10:06:31 -0500

    Store all cateogory data in csv

--- data-raw/recipes.R
+++ data-raw/recipes.R
@@ -3,13 +3,11 @@ library(readr)
 library(tidyr)
 
 # scp had.co.nz://home/hadley/public/recipes.had.co.nz/db/recipes.sqlite3 .
-src <- src_sqlite("data-raw/recipes.sqlite3")
+recipes <- src_sqlite("data-raw/recipes.sqlite3") %>% 
+  tbl("recipes") %>% 
+  collect()
 
-categories <- src %>%
-  tbl("categories") %>% 
-  collect() %>%
-  rename(category = name, category_id = id) %>% 
-  left_join(read_csv("data-raw/categories.csv"), "category")
+categories <- read_csv("data-raw/categories.csv")
 
 slug <- function(x) {
   x <- tolower(x)
@@ -20,9 +18,7 @@ slug <- function(x) {
   x <- make.unique(x, "-")
 }
 
-recipes <- src %>%
-  tbl("recipes") %>%
-  collect() %>% 
+recipes <- recipes %>% 
   replace_na(list(category_id = 12)) %>%
   left_join(categories, by = "category_id") %>% 
   mutate(
