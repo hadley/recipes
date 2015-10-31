082a1bf41cb62e5e2f87dfdda374f0438ab6d2ea
hadley <h.wickham@gmail.com>
2015-10-31 10:04:36 -0500

    Build category navigation

--- R/render.R
+++ R/render.R
@@ -1,15 +1,33 @@
-
-md_to_html <- function(input) {
-  tmp_in <- tempfile()
-  tmp_out <- tempfile()
+write_site <- function() {
+  if (!file.exists("www"))
+    dir.create("www")
   
-  on.exit(unlink(c(tmp_in, tmp_out), force = TRUE))
-  writeLines(input, tmp_in)
-  
-  pandoc_convert(tmp_in, to = "html", from = "markdown", output = tmp_out)
-  
-  read_lines(tmp_out)
+  write_index()
 }
+
+write_index <- function() {
+  body <- md_to_html(read_lines("recipes/index.md"))
+  page <- render_page("All recipes", category_nav(), body)
+  writeLines(page, "www/index.html")
+}
+
+category_nav <- function(cur = NULL) {
+  bullets <- paste0(
+    "<li><a href='/", categories$path, "'>", categories$category, "</a></li>"
+  )
+
+  if (!is.null(cur)) {
+    cur <- cur == categories$path
+    bullets[cur] <- paste0("<li>", categories$category[cur], "</li>")
+  }
+  
+  paste0(
+    "<ul>\n",
+    paste0("  ", bullets, "\n", collapse = ""),
+    "</ul>\n"
+  )
+}
+
 
 render_page <- function(title, navigation, body) {
   layout <- read_file("templates/layout.html")
@@ -21,15 +39,3 @@ render_page <- function(title, navigation, body) {
   )
 }
 
-write_index <- function() {
-  body <- md_to_html(read_lines("recipes/index.md"))
-  page <- render_page("All recipes", "", body)
-  writeLines(page, "www/index.html")
-}
-
-write_site <- function() {
-  if (!file.exists("www"))
-    dir.create("www")
-  
-  write_index()
-}
--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,14 @@
 `%?%` <- function(x, y) if (length(x) > 0) y
 `%||%` <- function(x, y) if (length(x) > 0) x else y
 
+md_to_html <- function(input) {
+  tmp_in <- tempfile()
+  tmp_out <- tempfile()
+  
+  on.exit(unlink(c(tmp_in, tmp_out), force = TRUE))
+  writeLines(input, tmp_in)
+  
+  pandoc_convert(tmp_in, to = "html", from = "markdown", output = tmp_out)
+  
+  read_file(tmp_out)
+}
